1a194c6feb4064292aefe77fdd4f87407c84bd0b
马子喻 <2246518178@qq.com>
2023-05-13 23:16:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import ddddocr
-from selenium.common.exceptions import NoSuchElementException, ElementNotSelectableException
+from selenium.common.exceptions import NoSuchElementException, ElementNotSelectableException, ElementNotInteractableException
 
 
 def ocr(file):
@@ -17,9 +17,15 @@ def ocr(file):
 if __name__ == '__main__':
     browser = webdriver.Firefox()
     browser.get("https://cas.sysu.edu.cn/cas/login?service=https%3A%2F%2Fjwxt.sysu.edu.cn%2Fjwxt%2Fapi%2Fsso%2Fcas%2Flogin%3Fpattern%3Dstudent-login")
-    input("登录后进入评教页面，按回车继续")
+    browser.find_element(By.XPATH, '//img[@id="captchaImg"]').screenshot("t.png")
+    f = open("t.png", "rb+")
+    data = f.read()
+    browser.find_element(By.XPATH, "//input[@placeholder='验证码']").send_keys(ocr(data))
+    input("登录，按回车继续")
+    browser.find_element(By.XPATH, "//div[text()='我的评教']").click()
     window_handles = browser.window_handles
     browser.switch_to.window(window_handles[1])
+    time.sleep(0.5)
     while True:
         try:
             browser.find_element(By.XPATH, "//div[contains(@class,'cz-bo')]/div[1]//button").click()
@@ -38,18 +44,32 @@ if __name__ == '__main__':
         for i in range(1, elements):
             browser.find_element(By.XPATH, "//tr[contains(@class,'ant-table-row-level-0')][" + str(i) + "]//input").send_keys(browser.find_element(By.XPATH, "//tr[contains(@class,'ant-table-row-level-0')][" + str(i) + "]/td[3]").text)
         browser.find_element(By.XPATH, "//span[text()='提 交']/..").click()
-        time.sleep(0.5)
         try:
-            browser.find_element(By.XPATH, "//input[@placeholder='请输入验证码']")
+            WebDriverWait(browser, 10, 0.5).until_not(EC.presence_of_element_located((By.XPATH, "//i[contains(@class,'anticon-loading')]")))
+        except Exception as e:
+            print (type(e),"::",e)
+        try:
+            browser.find_element(By.XPATH, "//input[@placeholder='请输入验证码']").click()
         except NoSuchElementException:
             print("成功，无验证码")
         else:
-            browser.find_element(By.XPATH, '//img[@alt="验证码"]').screenshot("t.png")
+            try:
+                ele = browser.find_element(By.XPATH, '//img[@alt="验证码"]')
+            except NoSuchElementException:
+                input("有问题待解决")
+            else:
+                try:
+                    ele.screenshot("t.png")
+                except Exception as e:
+                    input(e)
             f = open("t.png", "rb+")
             data = f.read()
             browser.find_element(By.XPATH, "//input[@placeholder='请输入验证码']").send_keys(ocr(data))
             browser.find_element(By.XPATH, "//span[text()='提 交']/..").click()
-            time.sleep(0.5)
+            try:
+                WebDriverWait(browser, 10, 0.5).until_not(EC.presence_of_element_located((By.XPATH, "//i[contains(@class,'anticon-loading')]")))
+            except Exception as e:
+                print (type(e),"::",e)
             try:
                 browser.find_element(By.XPATH, "//input[@placeholder='请输入验证码']")
             except NoSuchElementException:
